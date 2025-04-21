63a6863243a7dc90fff2f2099ce1aa2c079fef57
experientator <vladstarjinskji@gmail.com>
2025-04-21 23:52:29 +0300

    Update views.py

--- rost_proj/config/views.py
+++ rost_proj/config/views.py
@@ -55,10 +55,10 @@ def export_facts_to_csv(request):
 
     writer = csv.writer(response)
     writer.writerow([
-        'Client IP', 'Journal Name', 'User ID', 'Request Type', 'API Name',
+        'Date','Client IP', 'Journal Name', 'User ID', 'Request Type', 'API Name',
         'Protocol Version', 'Result Code', 'Result Time', 'Result Byte',
         'Referer Name', 'OS', 'Kernel', 'Rendering Engine', 'Engine Version',
-        'HTML Compatibility', 'Browser', 'Browser Version', 'Date'
+        'HTML Compatibility', 'Browser', 'Browser Version'
     ])
 
     facts = Facts_table.objects.all().select_related(
@@ -68,24 +68,24 @@ def export_facts_to_csv(request):
 
     for fact in facts:
         writer.writerow([
-            fact.id_client.ip_client if fact.id_client else '',
-            fact.id_client.journal_name if fact.id_client else '',
-            fact.id_client.user_id if fact.id_client else '',
-            fact.id_type.type_name if fact.id_type else '',
-            fact.id_API.api_name if fact.id_API else '',
-            fact.id_protocol.p_name if fact.id_protocol else '',
-            fact.id_result.code.code_name if fact.id_result and fact.id_result.code else '',
-            fact.id_result.result_time if fact.id_result else '',
-            fact.id_result.result_byte if fact.id_result else '',
-            fact.id_referer.ref_name if fact.id_referer else '',
-            fact.id_agent.os if fact.id_agent else '',
-            fact.id_agent.krnl if fact.id_agent else '',
-            fact.id_agent.ren_eng if fact.id_agent else '',
-            fact.id_agent.eng_ver if fact.id_agent else '',
-            fact.id_agent.html_cmpbl if fact.id_agent else '',
-            fact.id_agent.browser if fact.id_agent else '',
-            fact.id_agent.browser_ver if fact.id_agent else '',
-            fact.date if fact.date else ''
+            fact.date if fact.date else '-'
+            fact.id_client.ip_client if fact.id_client else '-',
+            fact.id_client.journal_name if fact.id_client else '-',
+            fact.id_client.user_id if fact.id_client else '-',
+            fact.id_type.type_name if fact.id_type else '-',
+            fact.id_API.api_name if fact.id_API else '-',
+            fact.id_protocol.p_name if fact.id_protocol else '-',
+            fact.id_result.code.code_name if fact.id_result and fact.id_result.code else '-',
+            fact.id_result.result_time if fact.id_result else '-',
+            fact.id_result.result_byte if fact.id_result else '-',
+            fact.id_referer.ref_name if fact.id_referer else '-',
+            fact.id_agent.os if fact.id_agent else '-',
+            fact.id_agent.krnl if fact.id_agent else '-',
+            fact.id_agent.ren_eng if fact.id_agent else '-',
+            fact.id_agent.eng_ver if fact.id_agent else '-',
+            fact.id_agent.html_cmpbl if fact.id_agent else '-',
+            fact.id_agent.browser if fact.id_agent else '-',
+            fact.id_agent.browser_ver if fact.id_agent else '-',
         ])
 
     return response
